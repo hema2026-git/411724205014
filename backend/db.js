609400aea94748faac70c11843ac84917d055b6b
unknown <cvhema2007@gmail.com>
2026-08-04 13:02:40 +0530

    Add modified files; update .gitignore; untrack node_modules

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "hema@15062007",
-    database: "notes_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
